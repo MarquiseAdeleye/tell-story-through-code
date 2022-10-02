aefcd6ad0d3f7d453c8b3cfddd9fc3e1d460855c
MarquiseAdeleye <marquiseadeleye@gmail.com>
2022-10-02 12:25:48 -0400

    added the homepage
    Added the initial homepage file to the github.

---
+++ Homepage.xcplaygroundpage/Contents.swift
@@ -0,0 +1,58 @@
+import UIKit
+import SwiftUI
+import PlaygroundSupport
+
+/*This is where I am setting the constants that will hold the color
+ scheme for the story
+ */
+let darkestGreen = Color(#colorLiteral(red: 0.12208087, green: 0.1928047538, blue: 0.2179225683, alpha: 1))
+let darkGreem = Color(#colorLiteral(red: 0, green: 0.3334578872, blue: 0.341104269, alpha: 1))
+let lightGreen = Color(#colorLiteral(red: 0.1709093451, green: 0.6570991278, blue: 0.607393682, alpha: 1))
+let lightestGreen = Color(#colorLiteral(red: 0.6796731353, green: 0.8603450656, blue: 0.7540082335, alpha: 1))
+let gold = Color(#colorLiteral(red: 1, green: 0.7618391514, blue: 0.0282842014, alpha: 1))
+
+
+/*This will be the initial view for the hompage*/
+struct Homepage: View{
+    var body: some View{
+        VStack{
+            Text("Le Renaissance Detroit")
+            Rectangle()
+                .fill(gold)
+                .frame(width: 375.0, height: 2.0)
+            HStack{
+                Image(uiImage: UIImage(imageLiteralResourceName: "Placeholder.png"))
+                    .resizable()
+                    .scaledToFit()
+                    .clipShape(Circle())
+                Image(uiImage: UIImage(imageLiteralResourceName: "Placeholder.png"))
+                    .resizable()
+                    .scaledToFit()
+                    .clipShape(Circle())
+                Image(uiImage: UIImage(imageLiteralResourceName: "Placeholder.png"))
+                    .resizable()
+                    .scaledToFit()
+                    .clipShape(Circle())
+                Image(uiImage: UIImage(imageLiteralResourceName: "Placeholder.png"))
+                    .resizable()
+                    .scaledToFit()
+                    .clipShape(Circle())
+                Image(uiImage: UIImage(imageLiteralResourceName: "Placeholder.png"))
+                    .resizable()
+                    .scaledToFit()
+                    .clipShape(Circle())
+            }
+        }
+        .padding()
+        .background(Image(uiImage: UIImage(imageLiteralResourceName: "MapOfDetroit.png")).resizable().scaledToFill()
+            .overlay(darkGreem)
+            .opacity(0.5)
+        )
+    }
+}
+
+
+PlaygroundPage.current.setLiveView(
+    Homepage()
+        .frame(width: 400.0, height: 600.0)
+)
